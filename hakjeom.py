c2c5bbf6b893b6569a1b7957a52526e0285dc6e6
joas24 <joas24@naver.com>
2023-03-30 16:27:28 +0900

    array

--- hakjeom.py
+++ hakjeom.py
@@ -1,14 +1,28 @@
-print('작업을 선택하세요.\n1. 입력\n2. 계산')
-n = int(input())
+
+grade = []
 while True:
+    print('작업을 선택하세요.\n1. 입력\n2. 계산')
+    n = int(input())
     if n == 1:
         print("학점을 입력하세요:")
-        ha = int(input())
+        gpa = int(input())
         print("평점을 입력하세요:")
-        pyo = input()
+        sco = input()
+        score = 0 
+        if sco !="F": score = 69-ord(sco[0]) if len(sco) == 1 else 69-ord(sco[0])+0.5 
+        grade.append([gpa,score]) 
         print("입력되었습니다.")
     else:
-        a = '3'
-        print(f'제출용: {a}학점(GPA: {a})\n열람용: {a}학점(GPA: {a})\n')
+        num_submit = 0
+        num_view = 0
+        gpa_submit = 0
+        gpa_view = 0
+        for i in range(len(grade)):
+            if grade[i][1] != 0:
+                num_submit += grade[i][0]
+                gpa_submit += grade[i][0]*grade[i][1]
+            num_view += grade[i][0]
+            gpa_view += grade[i][0]*grade[i][1]
+        print(f'제출용: {num_submit}학점(GPA: {gpa_submit/num_submit})\n열람용: {num_view}학점(GPA: {gpa_view/num_view})\n')
         print('프로그램을 종료합니다.')
         break
