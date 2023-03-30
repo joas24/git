b3e2d0498e0bea154d52da631f5c98319111b9b8
joas24 <joas24@naver.com>
2023-03-30 16:42:57 +0900

    no_array

--- hakjeom.py
+++ hakjeom.py
@@ -1,5 +1,6 @@
-
-grade = []
+gpa_sum = 0
+gpa_f = 0
+score_sum = 0
 while True:
     print('작업을 선택하세요.\n1. 입력\n2. 계산')
     n = int(input())
@@ -9,20 +10,15 @@ while True:
         print("평점을 입력하세요:")
         sco = input()
         score = 0 
-        if sco !="F": score = 69-ord(sco[0]) if len(sco) == 1 else 69-ord(sco[0])+0.5 
-        grade.append([gpa,score]) 
-        print("입력되었습니다.")
+        if sco !="F": 
+            score = 69-ord(sco[0]) if len(sco) == 1 else 69-ord(sco[0])+0.5 
+            gpa_sum += gpa
+            score_sum += gpa*score
+        else:
+            gpa_f += gpa   
+        print("입력되었습니다.\n")
+
     else:
-        num_submit = 0
-        num_view = 0
-        gpa_submit = 0
-        gpa_view = 0
-        for i in range(len(grade)):
-            if grade[i][1] != 0:
-                num_submit += grade[i][0]
-                gpa_submit += grade[i][0]*grade[i][1]
-            num_view += grade[i][0]
-            gpa_view += grade[i][0]*grade[i][1]
-        print(f'제출용: {num_submit}학점(GPA: {gpa_submit/num_submit})\n열람용: {num_view}학점(GPA: {gpa_view/num_view})\n')
+        print(f'제출용: {gpa_sum}학점(GPA: {score_sum/gpa_sum})\n열람용: {gpa_sum+gpa_f}학점(GPA: {score_sum/(gpa_sum+gpa_f)})\n')
         print('프로그램을 종료합니다.')
         break
