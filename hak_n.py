d41de3cb12a28f42c20ba10a5f025282b29cf71e
joas24 <joas24@naver.com>
2023-05-04 16:37:54 +0900

    end

--- hak_n.py
+++ hak_n.py
@@ -1,31 +1,29 @@
-import random
-sbjs = {}
-score = []
-score_re = []
-
 courses = []
 class CourseInfo:
-    
-    global courses
 
     def __init__(self, name, gpa, grade):
         self.name = name
-        self.gpa = gpa
+        self.gpa = int(gpa)
         self.grade = grade
-        courses.append(name)
         
+    def __str__(self):
+        return self.name
+    
     def call(self):
         print(f"[{self.name}] {self.gpa}학점 : {self.grade}")
 
     def call_all():
-        for i in courses:
-            globals()[i].call()
+        for c in courses:
+            c.call()
 
 
 def gradeToNum(grade):
     if grade[0] == 'F': return 0
     if len(grade)==2: return 69-ord(grade[0])+0.5  
     else: return 69-ord(grade)
+
+def clas(str):
+    return globals()[str]
 
 while True:
     print("작업을 선택하세요.\n1. 입력\n2. 출력\n3. 조회\n4. 계산\n5. 종료")
@@ -35,18 +33,19 @@ while True:
             print("과목명, 학점, 평점을 입력하세요 : ")
             sbj_name, sbj_gpa, sbj_grade = input().split(',')
 
-            if sbj_name in courses:#재수강 과목일 경우
-                score_re.append((n,sbj_gpa,sbj_grade))
-            else: #신규 과목일 경우
+            if sbj_name not in [str(i) for i in courses]:
                 globals()[sbj_name] = CourseInfo(sbj_name, sbj_gpa, sbj_grade)
+                courses.append(clas(sbj_name))
 
+            elif gradeToNum(sbj_grade) > gradeToNum(clas(sbj_name).grade):
+                clas(sbj_name).grade = sbj_grade
         
         case 2:
             CourseInfo.call_all()
 
         case 3: 
             course_name = input("과목명을 입력하세요 : ")
-            if course_name in courses:
+            if course_name in [str(i) for i in courses]:
                 globals()[course_name].call()
             else: print("해당하는 과목이 없습니다.")
 
@@ -54,19 +53,13 @@ while True:
             gpaSum = 0
             gpa_f = 0
             scoreSum = 0
-            good = 0
-            for i in range(len(score)):
-                gpaSum += score[i][1]
-                for j in range(len(score_re)):
-                    if score_re[j][0] == score[i][0]: #재수강을 했다!
-                        if gradeToNum(score_re[j][2]) > gradeToNum(score[i][2]): #재수강 평점이 더 높다!
-                            scoreSum += gradeToNum(score_re[j][2])*score[i][1]
-                            good = 1 #좋다!
-                        break
-                if not good:
-                    scoreSum += gradeToNum(score[i][2])*score[i][1]
-                    if score[i][2] == 'F': gpa_f += score[i][1]
-            print(f"제출용: {gpaSum-gpa_f}학점(GPA: {scoreSum/(gpaSum-gpa_f)})\n열람용: {gpaSum}학점(GPA: {scoreSum/gpaSum})")
+            for sbj in courses:
+                if gradeToNum(sbj.grade): 
+                    gpaSum += sbj.gpa
+                    scoreSum += sbj.gpa * gradeToNum(sbj.grade)
+                else: gpa_f += sbj.gpa
+                
+            print(f"제출용: {gpaSum}학점(GPA: {scoreSum/gpaSum})\n열람용: {gpaSum+gpa_f}학점(GPA: {scoreSum/(gpaSum+gpa_f)})")
             break
 
         case 5:
